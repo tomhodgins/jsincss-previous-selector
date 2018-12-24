d0df259232157303a5fe01555fcb59fd44b0ad2b
Tommy Hodgins <tomhodgins@gmail.com>
2018-12-24 15:52:01 -0500

    Refactor to be cleaner/faster

--- index.browser.js
+++ index.browser.js
@@ -1,17 +1,13 @@
 function previous(selector, rule) {
-
-  return Array.from(document.querySelectorAll(selector))
-
+  const attr = selector.replace(/\W/g, '')
+  const result = Array.from(document.querySelectorAll(selector))
     .filter(tag => tag.previousElementSibling)
-
-    .reduce((styles, tag, count) => {
-
-      const attr = selector.replace(/\W/g, '')
-
-      tag.previousElementSibling.setAttribute(`data-prev-${attr}`, count)
-      styles += `[data-prev-${attr}="${count}"] { ${rule} }\n`
-      return styles
-
-    }, '')
-
+    .reduce((output, tag, count) => {
+      output.add.push({tag: tag.previousElementSibling, count: count})
+      output.styles.push(`[data-prev-${attr}="${count}"] { ${rule} }`)
+      return output
+    }, {add: [], remove: [], styles: []})
+  result.add.forEach(tag => tag.tag.setAttribute(`data-prev-${attr}`, tag.count))
+  result.remove.forEach(tag => tag.setAttribute(`data-prev-${attr}`, ''))
+  return result.styles.join('\n')
 }
--- source/previous.sexp
+++
@@ -1,7 +0,0 @@
-mixin('previous', ['selector', 'rule'],
-  returnValue('Array.from(document.querySelectorAll(selector))',
-    filterFunc('tag.previousElementSibling',
-      reduceFunc(
-        createAttribute(['selector'],
-          addAttribute('tag.previousElementSibling', 'prev',
-            addRule('', '', 'prev')))))))
